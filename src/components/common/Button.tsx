243da8645663f31a2709427391c7b843be2c7ee4
benji011 <wolfnun011@gmail.com>
2021-04-30 16:20:13 +0900

    fix: filter chatrooms based on who made them

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,14 @@
 import { IButton } from '~/models/common/IButton'
 
 export const Button = (props: IButton) => {
-  const { className, text, icon, onClick } = props
+  const { className, text, icon, onClick, isDisabled } = props
   return (
-    <button className={className} type="button" onClick={onClick}>
+    <button
+      className={className}
+      type="button"
+      onClick={onClick}
+      disabled={isDisabled}
+    >
       <span>{text}</span>
       <span className="icon">
         <i className={icon} />
--- src/models/common/IButton.ts
+++ src/models/common/IButton.ts
@@ -4,5 +4,6 @@ export interface IButton {
   className: string
   text?: string
   icon: string
+  isDisabled?: boolean
   onClick?: any //() => void // TODO: fix later
 }
