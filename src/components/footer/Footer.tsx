a278fbd84b73f9e08d280efb145c926140b0f4d9
benji011 <wolfnun011@gmail.com>
2021-04-30 16:53:56 +0900

    fix: Footer content

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,25 +6,34 @@ export const Footer = () => {
       <div className="content has-text-centered">
         <p>
           <strong>Yggdrasil</strong> by{' '}
-          <a className="footer-a" href="https://benjaminlo.io">
+          <a
+            className="footer-a"
+            href="https://benjaminlo.io"
+            target="_blank"
+            rel="noreferrer"
+          >
             Benjamin Lo
           </a>
           . The source code is licensed{' '}
           <a
             className="footer-a"
             href="http://opensource.org/licenses/mit-license.php"
+            target="_blank"
+            rel="noreferrer"
           >
             MIT
-          </a>
+          </a>{' '}
           . <br />
-          The website content is licensed{' '}
+          The website is also available on{' '}
           <a
             className="footer-a"
-            href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
+            href="https://github.com/benji011/yggdrasil-chatroom"
+            target="_blank"
+            rel="noreferrer"
           >
-            CC BY NC SA 4.0
-          </a>
-          .
+            GitHub.
+          </a>{' '}
+          Feel free to send me a pull request!
         </p>
       </div>
     </footer>
